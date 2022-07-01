2e00543279d9b0724cc8e66b5bec8161054e075a
Toka-1992 <t.elwetedy@gmail.com>
2022-07-01 07:53:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - print the size of various types on the computer it is compiled and run on
+* main - print the size of various types on the computer
 *
 * Description: using the main function
 * Return: 0
