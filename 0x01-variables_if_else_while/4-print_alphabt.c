08f8c252c1a9d340c1d6bf689e9282b47e48a126
Toka-1992 <t.elwetedy@gmail.com>
2022-07-01 17:48:29 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - print if the number is postive, zero, or negative
+* main - print if alphapet lower case except e and q
 *
 * Description: using the main function
-* this program prints "Programming is positive, zero, or negative
+* this program prints "all alphapets in lowercase exept e and q
 * Return: 0
 */
 int main(void)
