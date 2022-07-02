2060ac349ab4ce0df035341ce9aeb1d53b2345ff
Toka-1992 <t.elwetedy@gmail.com>
2022-07-01 17:28:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print if the alphapet is lower case
+*
+* Description: using the main function
+* This prog prints alphapets in lower case
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
