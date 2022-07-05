eeb673f28c0d7761eddba6b6c9e19031886a4ac5
Toka-1992 <t.elwetedy@gmail.com>
2022-07-05 16:03:41 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,12 @@
 */
 void print_alphabet_x10(void)
 {
-int i = 0;
-char letter = a;
-while (i < 10)
+char i;
+int j;
+for (j = 0; j < 10; j++)    
 {
-letter = 'a';
-while (letter <= 'z')
-{
-_putchar = (letter);
-letter++
-}
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 _putchar('\n');
-i++;
-}
+} 
 }
